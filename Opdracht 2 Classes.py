eaeb9f04bd8a55f61ed79afe286b91badd70e02f
Guidoge3 <70884054+Guidoge3@users.noreply.github.com>
2020-12-11 17:24:46 +0100

    changed the code of my assignment
    made the code work

--- Opdracht 2 Classes.py
+++ Opdracht 2 Classes.py
@@ -2,58 +2,62 @@ import sys
 import pygame
 from pygame.locals import *
 import random
+import time
 
-class Babroi(Characters):
+class Babroi:
 
     Lives = 4
     Speed = 25
-    Points = 0
+    Points = 5
     item = None
 
     def __init__(self):
         #Constructor functie.
+        self.Speed = 25
+        print("Babroi is created out of atoms!")
 
     def Walk(self):
-        super().Walk()
-        #instructies lopen.
+        print("snelheid van Babroi:", self.Speed)
 
-    def die(self):
-        #dood
+    def die (self):
+        print("Babroi is dood gegaan")
+    
+
+    def levenserbij (self):
+        if (Babroi.Points == 100):
+            Babroi.Lives += 1
+
+        elif (Babroi1.Points != 100):
+            while True:
+                time.sleep(5)
+                print("points is: ", Babroi1.Points)
 
 Babroi1 = Babroi()
 Babroi2 = Babroi()
-Babroi1.points += 15
+Babroi1.Points += 15
 Babroi2.Walk()
 
 
-class enemy(Characters):
-    
-    Lives = 1
-    Speed = 13
-    Points = 15
-    
-    def __init__(self):
+class enemy(Babroi):
+        
+    Points = 3
+
+    def __init__(self, Speed, Lives):
+        self.Speed = super().Speed
+        self.Lives = super().Lives
+        print("enemy is ontstaan")
         #constructor
-    
-    def Walk(self):
-        #lopen
 
-    def die(self):
-        #dood
+    def Walk(self, newSpeed):
+        # newspeed = self.Speed
+        print("enemy loopt met snelheid: ", newSpeed)
 
-enemy1 = enemy()
-enemy2 = enemy()
-enemy1.points -= 15
-enemy2.Walk()
+enemy1 = enemy(15, 1)
+enemy2 = enemy(5, 3)
+enemy1.Walk(3)
 
-class Characters:
-
-    Speed = 25
-    sprite = ###
-    Points = 15
-
-Walk(self):
-    #waggelen
-die(self):
-    #dood
-
+print(Babroi1.Points)
+print(Babroi1.Speed)
+print(Babroi1.Lives)
+print(Babroi1.Points)
+print(enemy1.Walk(3))
